7bec8966698809d6a4996e067bfc0c26053b4f97
D. Caine Winters <caine.winters@outlook.com>
2023-07-30 18:57:47 -0400

    Markdown generation work and Inquirer completion

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
 // TODO: Include packages needed for this application
+require('dotenv').config();
 const inquirer = require('inquirer');
-const fs = require('fs');
+const wtf = require('./utils/makeFile');
 const md = require('./utils/generateMarkdown');
-
-// TODO: Create an array of questions for user input
-const questions = [
-    {type: `input`, message: `Enter a project title>> `,    name: `Title`},
-    {type: `input`, message: `Project description>> `,      name: `Description`},
-    {type: `input`, message: `Table of Contents>> `,        name: `ToC`},
-    {type: `input`, message: `Installation>> `,             name: `Install`},
-    {type: `input`, message: `Usage>> `,                    name: `Usage`},
-    {type: `list`,  message: `License>> `,                  name: `License`, choices: [`MIT`, `ISC`, `Apache`, `BSD`, `<none>`]},
-    {type: `input`, message: `Contributing>> `,             name: `Contributors`},
-    {type: `input`, message: `Tests>> `,                    name: `Tests`},
-    {type: `input`, message: `Questions>> `,                name: `Questions`},
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const { Octokit } = require('octokit');
+const questions = [];
+const licensesGH = [];
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
+    getLicenses().then(() => {
+        // Build list of license names for Inquirer select-from-list input
+        const questionsRay = [];
+        for(let i = 0; i < licensesGH.length; i++){
+            questionsRay.push(licensesGH[i].name);
+        }
+
+        questions.push(
+            {type: `input`, message: `Enter a project title>> `,    name: `Title`},
+            {type: `input`, message: `Project description>> `,      name: `Description`},
+            {type: `input`, message: `Table of Contents>> `,        name: `ToC`},
+            {type: `input`, message: `Installation>> `,             name: `Install`},
+            {type: `input`, message: `Usage>> `,                    name: `Usage`},
+            {type: `list`,  message: `License>> `,                  name: `License`, choices: questionsRay},
+            {type: `input`, message: `Contributing>> `,             name: `Contributors`},
+            {type: `input`, message: `Tests>> `,                    name: `Tests`},
+            {type: `input`, message: `Questions>> `,                name: `Questions`},
+        );
+    }).then(() => {
+        inquirer.prompt(questions)
         .then((response) => {
-            // console.log(`Inquirer .then>> ${response}`);
-            const mdStr = md(response);
-            console.log(`generateMarkdown output:\n${mdStr}\n==============================`);
+            const mdStr = md(response, licensesGH);
+            // console.log(`generateMarkdown output:\n${mdStr}\n==============================`);
+            wtf(mdStr);
         })
-        .catch((e) => {
-            console.log(`Inquirer had a problem :(\t>> ${e}`);
-            // let str = `Name:\n\t ${r[0]} \nLanguages talked: \n\t`;
-            // r[1].forEach((element) => { str = `${str} ${element} `;});
-            // str = `${str}\nPreferred communido:\n\t ${r[2]}`;
-            // fs.writeFile(`${r[0]}.txt`, str, (e) =>{ e ? console.log(e) : console.log("Win");});
-        });
+        .catch((e) => { console.log(`Inquirer had a problem :(\t>> ${e}`);});
+    });
+}
+
+
+
+async function getLicenses(){
+    const octokit = new Octokit({
+        auth: process.env.GH_TOKEN
+      })
+      
+    const licenses = await octokit.request('GET /licenses', {
+        headers: {'X-GitHub-Api-Version': '2022-11-28'}
+        })
+    // Populates all licenses' info
+    licensesGH.push(...licenses.data);
 }
 
 // Function call to initialize app
 init();
-
-
-
---
+++ utils/makeFile.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+function writeToFile(data){
+    // From StackOverflow: https://stackoverflow.com/questions/57742157/node-js-how-to-find-the-desktop-path
+    const homeDir = require('os').homedir();
+    const desktopDir = `${homeDir}/Desktop/superDuperREADME.md`;
+    // console.log(`fileName: ${fileName}\tdata: ${data}`);
+    fs.writeFile(desktopDir, data, (e) => { e ? console.log(`wtf(e): ${e}`) : console.log(`win`);});
+}
+
+module.exports = writeToFile;
+
+
+
